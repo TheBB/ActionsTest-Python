629de9d0af2364f3a1cd17e8f6e969b5cce2271d
Eivind Fonn <evfonn@gmail.com>
2020-11-16 17:26:27 +0100

    Edit setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from Cython.Build import cythonize
 
 setup(
     name='TheBB-TestPackage',
-    version='1.0.0',
+    version='3.0.0',
     maintainer='Eivind Fonn',
     maintainer_email='evfonn@gmail.com',
     packages=['thebb_testpackage'],
